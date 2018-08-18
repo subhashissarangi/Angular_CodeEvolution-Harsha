c1bf0b544490d9a9048f5720ff43d17de59d4f4c
subhashis <sarangi.subhashis@gmail.com>
2018-08-18 09:49:59 +0530

    Code commit

---
+++ CodeEvolution/src/app/test/test.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+
+  <div>
+  <h1>Hello</h1>
+  </div>
+<input [id]= myId type="text" value="subhashis">
+`,
+  styles: [
+    `
+   div{
+   color : red;
+}
+`
+  ]
+})
+export class TestComponent implements OnInit {
+  
+  public myId = "testId";
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
